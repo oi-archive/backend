09f7eee5de52326a3d333217e4760120b73ec4c8
WAAutoMaton <a547673135@gmail.com>
2021-08-30 06:54:44 +0000

    add flag port

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-contrib/cache/persistence"
 	"github.com/gin-gonic/gin"
 	"log"
+	"strconv"
 	"time"
 )
 
@@ -16,8 +17,12 @@ func PanicIfError(err error) {
 }
 func main() {
 	cliPath := flag.String("source", "./source", "source 仓库路径")
+	port := flag.Int("port", 10001, "listening port")
 	flag.Parse()
 	BasePath = *cliPath
+	if *port <= 1 || *port > 65535 {
+		log.Fatalln("invalid port")
+	}
 
 	PanicIfError(UpdateData())
 
@@ -30,5 +35,5 @@ func main() {
 	router.GET("/problem-set/:problemset/metadata", cache.CachePage(store, cacheTime, APIProblemSetMetadata))
 	router.GET("/problem-list/:problemset/:page", cache.CachePage(store, cacheTime, APIProblemSetPage))
 	router.GET("/problem/:problemset/:problem", cache.CachePage(store, cacheTime, APIProblem))
-	PanicIfError(router.Run("0.0.0.0:10001"))
+	PanicIfError(router.Run("0.0.0.0:" + strconv.Itoa(*port)))
 }
